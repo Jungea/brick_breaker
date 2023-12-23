f90670e219fad8973b20e1014bb124c56782aa08
Jungea <jungea2241@gmail.com>
2023-12-23 16:20:09 +0900

    벽 충돌 처리

--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -1,8 +1,13 @@
 import 'dart:ui';
 
+import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:brick_breaker/src/components/components.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/cupertino.dart';
 
-class Ball extends CircleComponent {
+class Ball extends CircleComponent
+    with CollisionCallbacks, HasGameReference<BrickBreaker> {
   final Vector2 velocity;
 
   Ball({
@@ -14,11 +19,31 @@ class Ball extends CircleComponent {
             anchor: Anchor.center,
             paint: Paint()
               ..color = const Color(0xff1e6091)
-              ..style = PaintingStyle.fill);
+              ..style = PaintingStyle.fill,
+            children: [CircleHitbox()]);
 
   @override
   void update(double dt) {
     super.update(dt);
     position += velocity * dt;
+  }
+
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is PlayArea) {
+      if (intersectionPoints.first.y <= 0) {
+        velocity.y = -velocity.y;
+      } else if (intersectionPoints.first.x <= 0) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.x >= game.width) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.y >= game.height) {
+        removeFromParent();
+      }
+    } else {
+      debugPrint('collision with $other');
+    }
   }
 }
